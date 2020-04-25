5c14d9c54ac4c74e56a78407d09a44727af9dd75
Julio Suriano <jjsuriano@susshicorp.com>
2020-04-25 12:02:19 -0600

    hide other steps

--- scripts/main.js
+++ scripts/main.js
@@ -7,48 +7,33 @@ const next_2 = document.querySelector('#next-2');
 const prev_3 = document.querySelector('#prev-3');
 const reset = document.querySelector('#reset');
 
-let counter = 0;
-
 const width = carousel_step[0].clientWidth;
 
-console.log(carousel_step);
+let option = 0;
 
 next_1.addEventListener('click', ()=> {
-    console.log("next-1 clicked")
-    
     carousel_container.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
     carousel_container.style.transform = 'translateX('+ -width*1 +'px)';
+    option = document.querySelector().
 });
 
 prev_2.addEventListener('click', ()=> {
-    console.log("prev-2 clicked")
-    
     carousel_container.style.transition = 'transform 0.4s ease-in-out';
-    counter--;
-    console.log(counter);
     carousel_container.style.transform = 'translateX('+ 0 +'px)';
 });
 
 next_2.addEventListener('click', ()=> {
-    console.log("next-2 clicked")
-    
     carousel_container.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
     carousel_container.style.transform = 'translateX('+ -width*2 +'px)';
 });
 
 prev_3.addEventListener('click', ()=> {
-    console.log("prev-3 clicked")
-    
     carousel_container.style.transition = 'transform 0.4s ease-in-out';
-    counter--;
     carousel_container.style.transform = 'translateX('+ -width*1 +'px)';
 });
 
 reset.addEventListener('click', ()=> {
-    console.log("reset clicked")
     carousel_container.style.transition = 'transform 0.4s ease-in-out';
-    counter--;
     carousel_container.style.transform = 'translateX('+ width*0 +'px)';
+    
 });
