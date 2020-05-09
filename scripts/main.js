4426023c367ed160a37e7ffa869db022f3b4f7f2
Julio Suriano <julios@vt.edu>
2020-05-09 16:46:08 -0600

    Finished step 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
     $('#starting-page').hide();
-    $('#input-page-1').hide();
+    $('#input-page').hide();
+    $('#input-box-2').hide();
+    $('#input-box-3').hide();
+    $('#input-box-4').hide();
     $('#results-page').hide();
 
     let option = 0;
+    let income = null;
+    let expenses = null;
+    let retirement = null;
+    let health = null;
+    let taxes = null;
 
     // = = = = = GENERAL = = = = 
 
@@ -23,25 +31,30 @@ $(document).ready(function(){
         return (neg ? "-$" : '$') + parseFloat(total, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString();
     }
 
-    function resultsHeader(option, income) {
+    function resultsHeader(option, income, expenses, health, retirement) {
         let header = "";
 
         if (option == 1) {
-            header = 'Si tus ingresos mensuales son de <span class="highlight-blue" id="result-income">'+ formatCurrency(income) +'</span>';
+            header = 'Tu salario mensual es <span class="highlight-blue">'+ formatCurrency(income) +'</span>.';
         }
 
         if (option == 2) {
-
+            
         }
 
         if (option == 3) {
-
+            if (expenses == 0 ) {
+                header = 'Tus ingresos en el año fueron <span class="highlight-blue">' + formatCurrency(income) + '</span> y no tuviste gastos en el año.';
+            } else {
+                header = 'Tus ingresos en el año fueron <span class="highlight-blue">' + formatCurrency(income) + '</span> y tus gastos en el año fueron <span class="highlight-blue">'+ formatCurrency(expenses) +'</span>.';
+            }
+            
         }
         return header;
 
     }
 
-    function resultsText(option, results) {
+    function resultsText1(results) {
         let salario = results[0];
         let isss = results[2];
         let afp = results[3];
@@ -51,13 +64,12 @@ $(document).ready(function(){
         let impuestos = results[7];
         let liquidos = formatCurrency(salario - isss - afp - impuestos);
 
-        if (option == 1) {
             let tramo = ""
 
             if (results[1] == 1) {               
                 tramo = "I";
-                let part1 = 'Tu aportación al <span class="highlight-red">ISSS</span> es de <span class="highlight-blue" id="result-isss">'+ formatCurrency(isss) + '</span> y tu aportación al <span class="highlight-red">AFP</span> es de <span class="highlight-blue" id="result-afp">'+ formatCurrency(afp) +'</span>. ';
-                let part2 = '<br><br>Esto significa que entras en el <span class="highlight-red" id="result-tramo">tramo ' + tramo + '</span> para el cálculo de tus impuestos. Si caes en este tramo <span class="highlight-blue">no tienes que pagar impuestos</span>.';
+                let part1 = 'Significa que tu aportación al <span class="highlight-red">ISSS</span> es de <span class="highlight-blue" id="result-isss">'+ formatCurrency(isss) + '</span> y tu aportación al <span class="highlight-red">AFP</span> es de <span class="highlight-blue" id="result-afp">'+ formatCurrency(afp) +'</span>. ';
+                let part2 = '<br><br>Esto me dice que entras en el <span class="highlight-red" id="result-tramo">tramo ' + tramo + '</span> para el cálculo de tus impuestos. Si caes en este tramo <span class="highlight-blue">no tienes que pagar impuestos</span>.';
                 let part3 = '<br><br>Si restamos todo esto de tu salario mensual, a ti te quedan <span class="highlight-blue" id="result-final">'+ liquidos +'</span> para comprar y ahorrar.';
                 return part1 + part2 + part3;
             } else if (results[1] == 2 ) {
@@ -68,22 +80,86 @@ $(document).ready(function(){
                 tramo = "IV";
             }
 
-            let part1 = 'Tu aportación al <span class="highlight-red">ISSS</span> es de <span class="highlight-blue" id="result-isss">'+ formatCurrency(isss) + '</span> y tu aportación al <span class="highlight-red">AFP</span> es de <span class="highlight-blue" id="result-afp">'+ formatCurrency(afp) +'</span>. ';
-            let part2 = '<br><br>Esto significa que entras en el <span class="highlight-red" id="result-tramo">tramo ' + tramo + '</span> para el cálculo de tus impuestos. Tus impuestos se dividen en dos partes, la cuota fija de <span class="highlight-red" id="result-cuota">' + cuota + '</span> y el <span class="highlight-red" id="result-porcentaje">' + porcentaje + '%</span> de <span class="highlight-red" id="result-porcion">' + exceso + '</span>, sumando esas dos partes nos da un total de <span class="highlight-blue" id="result-impuestos">' + formatCurrency(impuestos) + '</span> de <span class="highlight-red">impuestos</span>.';
+            let part1 = 'Significa que tu aportación al <span class="highlight-red">ISSS</span> es de <span class="highlight-blue" id="result-isss">'+ formatCurrency(isss) + '</span> y tu aportación al <span class="highlight-red">AFP</span> es de <span class="highlight-blue" id="result-afp">'+ formatCurrency(afp) +'</span>. ';
+            let part2 = '<br><br>Esto me dice que entras en el <span class="highlight-red" id="result-tramo">tramo ' + tramo + '</span> para el cálculo de tus impuestos. Tus impuestos se dividen en dos partes, la cuota fija de <span class="highlight-red" id="result-cuota">' + cuota + '</span> y el <span class="highlight-red" id="result-porcentaje">' + porcentaje + '%</span> de <span class="highlight-red" id="result-porcion">' + exceso + '</span>, sumando esas dos partes nos da un total de <span class="highlight-blue" id="result-impuestos">' + formatCurrency(impuestos) + '</span> de <span class="highlight-red">impuestos</span>.';
             let part3 = '<br><br>Si restamos todo esto de tu salario mensual, a ti te quedan <span class="highlight-blue" id="result-final">'+ liquidos +'</span> para comprar y ahorrar.';
+            return part1 + part2 + part3;
+    }
+
+    function resultsText2(results) {
+        let salario = results[0];
+        let gastos = results[1];
+        let isss = results[2];
+        let afp = results[3];
+        let porcentaje = parseInt(results[4]*100);
+        let exceso = formatCurrency(results[5]);
+        let cuota = formatCurrency(results[6]);
+        let retenido = results[7]
+        let impuestos = results[8];
+        let part1 = '';
+        let part2 = '';
+        let part3 = '';
+        
+        if (option == 2) {
+            if (results[9] == 1) {               
+                tramo = "I";
+                part1 = 'A tus gastos hay que agregarle los <span class="highlight-blue">'+ formatCurrency(1600) + '</span> automáticos que corresponden a <span class="highlight-red">colegiatura</span> y <span class="highlight-red">salud</span>.';
+                part2 = '<br><br>Esto me dice que entras en el <span class="highlight-red" id="result-tramo">tramo ' + tramo + '</span> para el cálculo de tus impuestos. Si caes en este tramo <span class="highlight-blue">no tienes que pagar impuestos</span>.';
+                part3 = '<br><br>Entonces tu recibirás todas tus retenciones, que son <span class="highlight-blue" id="result-final">'+ formatCurrency(retenido) +'</span> al final del año.';
+                return part1 + part2 + part3;
+            } else if (results[9] == 2 ) {
+                tramo = "II";
+            } else if (results[9] == 3 ) {
+                tramo = "III";
+            } else if (results[9] == 4 ) {
+                tramo = "IV";
+            }
+            let resultado = impuestos - retenido;
+            if (resultado < 0) {
+                resultado = resultado*-1;
+                part3 = '<br><br>Tus retenciones dan un total de <span class="highlight-blue">'+ formatCurrency(retenido) +'</span> y si sacas la diferencia te darás cuenta que recibirás <span class="highlight-blue">'+ formatCurrency(resultado) +'</span> al final del año.';
+            } else if (resultado > 0) {
+                part3 = '<br><br>Tus retenciones dan un total de <span class="highlight-blue">'+ formatCurrency(retenido) +'</span> y si sacas la diferencia te darás cuenta que te toca pagar <span class="highlight-blue">'+ formatCurrency(resultado) +'</span> al final del año.';
+            } else {
+                part3 = '<br><br>Tus retenciones dan un total de <span class="highlight-blue">'+ formatCurrency(retenido) +'</span>, entonces esto me dice que tu no debes ni te deben.';
+            }
+            part1 = 'A tus gastos hay que agregarle los <span class="highlight-blue">'+ formatCurrency(1600) + '</span> automáticos que corresponden a <span class="highlight-red">colegiatura</span> y <span class="highlight-red">salud</span>.';
+            part2 = '<br><br>Esto me dice que entras en el <span class="highlight-red" id="result-tramo">tramo ' + tramo + '</span> para el cálculo de tus impuestos. Tus impuestos se dividen en dos partes, la cuota fija de <span class="highlight-red" id="result-cuota">' + cuota + '</span> y el <span class="highlight-red" id="result-porcentaje">' + porcentaje + '%</span> de <span class="highlight-red" id="result-porcion">' + exceso + '</span>, sumando esas dos partes nos da un total de <span class="highlight-blue" id="result-impuestos">' + formatCurrency(impuestos) + '</span> de <span class="highlight-red">impuestos</span>.';
+
             return part1 + part2 + part3;
         }
 
-        if (option == 2) {
-
-        }
-
         if (option == 3) {
+            if (results[9] == 1) {               
+                tramo = "I";
+                part1 = 'A tus gastos hay que agregarle los <span class="highlight-blue">'+ formatCurrency(1600) + '</span> automáticos que corresponden a <span class="highlight-red">colegiatura</span> y <span class="highlight-red">salud</span>.';
+                part2 = '<br><br>Esto me dice que entras en el <span class="highlight-red" id="result-tramo">tramo ' + tramo + '</span> para el cálculo de tus impuestos. Si caes en este tramo <span class="highlight-blue">no tienes que pagar impuestos</span>.';
+                part3 = '<br><br>Entonces tu recibirás todas tus retenciones, que son <span class="highlight-blue" id="result-final">'+ formatCurrency(retenido) +'</span> al final del año.';
+                return part1 + part2 + part3;
+            } else if (results[9] == 2 ) {
+                tramo = "II";
+            } else if (results[9] == 3 ) {
+                tramo = "III";
+            } else if (results[9] == 4 ) {
+                tramo = "IV";
+            }
+            let resultado = impuestos - retenido;
+            if (resultado < 0) {
+                resultado = resultado*-1;
+                part3 = '<br><br>Tus retenciones dan un total de <span class="highlight-blue">'+ formatCurrency(retenido) +'</span> y si sacas la diferencia te darás cuenta que recibirás <span class="highlight-blue">'+ formatCurrency(resultado) +'</span> al final del año.';
+            } else if (resultado > 0) {
+                part3 = '<br><br>Tus retenciones dan un total de <span class="highlight-blue">'+ formatCurrency(retenido) +'</span> y si sacas la diferencia te darás cuenta que te toca pagar <span class="highlight-blue">'+ formatCurrency(resultado) +'</span> al final del año.';
+            } else {
+                part3 = '<br><br>Tus retenciones dan un total de <span class="highlight-blue">'+ formatCurrency(retenido) +'</span>, entonces esto me dice que tu no debes ni te deben.';
+            }
+            part1 = 'A tus gastos hay que agregarle los <span class="highlight-blue">'+ formatCurrency(1600) + '</span> automáticos que corresponden a <span class="highlight-red">colegiatura</span> y <span class="highlight-red">salud</span>.';
+            part2 = '<br><br>Esto me dice que entras en el <span class="highlight-red" id="result-tramo">tramo ' + tramo + '</span> para el cálculo de tus impuestos. Tus impuestos se dividen en dos partes, la cuota fija de <span class="highlight-red" id="result-cuota">' + cuota + '</span> y el <span class="highlight-red" id="result-porcentaje">' + porcentaje + '%</span> de <span class="highlight-red" id="result-porcion">' + exceso + '</span>, sumando esas dos partes nos da un total de <span class="highlight-blue" id="result-impuestos">' + formatCurrency(impuestos) + '</span> de <span class="highlight-red">impuestos</span>.';
 
+            return part1 + part2 + part3;
         }
     }
 
-    // = = = = = CON DEPENDENCIA = = = = = 
+    // = = = = = CON DEPENDENCIA MENSUAL = = = = = 
     function impuesto_planilla_mensual(salario) {
         let isss = 0
         let afp = 0;
@@ -130,6 +206,49 @@ $(document).ready(function(){
         let impuesto = (exceso * porcentaje) + cuota;
         return [salario, tramo, isss, afp, porcentaje, exceso, cuota, impuesto];
     }
+
+    // = = = = = IMPUESTOS ANUALES = = = = = 
+
+    function impuestos_anual(ingresos, gastos, isss, afp, retenido){
+
+        let salud = 800;
+        let colegiatura = 800;
+        let gravado = ingresos -isss - afp - salud - colegiatura - gastos;
+        let tramo = 0;
+        let porcentaje = 0;
+        let base = 0;
+        let cuota = 0;
+        let exceso = 0;
+
+        if (option == 3) {
+            retenido = ingresos*0.1;
+        }
+    
+        if (gravado <= 4064) {
+            tramo = 1
+            return [ingresos, gastos, isss, afp, 0, 0, 0, retenido, 0, tramo];
+        } else if (4064.01 <= gravado && gravado <= 9142.86) {
+            tramo = 2;
+            porcentaje = 0.1;
+            base = 4064;
+            cuota = 212.12;
+            exceso = gravado - base
+        } else if (9142.87 <= gravado && gravado <= 22857.14) {
+            tramo = 3;
+            porcentaje = 0.2;
+            base = 9142.86;
+            cuota = 720;
+            exceso = gravado - base
+        } else if (gravado >= 22857.15) {
+            tramo = 4;
+            porcentaje = 0.3;
+            base = 22857.14;
+            cuota = 3462.86;
+            exceso = gravado - base
+        }
+        let impuesto = (exceso * porcentaje) + cuota
+        return [ingresos, gastos, isss, afp, porcentaje, exceso, cuota, retenido, impuesto, tramo];
+    }
     
     $('#start').click(function(){
         $('#welcome').fadeOut(500, function(){
@@ -138,41 +257,103 @@ $(document).ready(function(){
     });
 
     $('#choice-1').click(function(){
+
+        $('#note-1').html($('#note-1').text().replace($('#note-1').text(), '<span class="highlight-blue">Tu salario mensual.</span> <br><br>Salario también es conocido como renumeración, sueldo o estipendio.'))
+
         $('#starting-page').fadeOut(500, function() {
-            $('#input-page-1').fadeIn(500);
+            $('#input-page').fadeIn(500, function() {
+                $('#input-1').focus();
+            });
         })
         option = 1;
     });
 
+    $('#choice-3').click(function(){
+
+        $('#note-1').html($('#note-1').text().replace($('#note-1').text(), '<span class="highlight-blue">Tus ingresos en todo el año.</span>'));
+
+        $('#starting-page').fadeOut(500, function() {
+            $('#input-page').fadeIn(500);
+        })
+        option = 3;
+    });
+
     $('#input-1').on('keypress',function(e) {
         income = $('#input-1').val();
-        if (income == '') {
-            income = null;
-        } else {
+        if (income != '') {
             income = parseFloat(income);
         }
         if(e.which == 13)  {
             if (option == 1 && income != null) {
-                $('#results-header').html($('#results-header').text().replace($('#results-header').text(), resultsHeader(1, income)));
-                $('#results-text').html($('#results-text').text().replace($('#results-text').text(), resultsText(1, impuesto_planilla_mensual(income))));
+                $('#results-header').html($('#results-header').text().replace($('#results-header').text(), resultsHeader(option, income, expenses, health, retirement)));
+                $('#results-text').html($('#results-text').text().replace($('#results-text').text(), resultsText1(impuesto_planilla_mensual(income))));
                 
-                $('#input-page-1').fadeOut(500, function(){
+                $('#input-page').fadeOut(500, function(){
                     $('#results-page').fadeIn(500);
                 })
                 document.activeElement.blur();
             }
 
-            if (option == 2 || option == 3) {
-                console.log(option);
+            if (option == 2) {
+                $('#note-2').html($('#note-2').text().replace($('#note-2').text(), '<span class="highlight-blue">Tus aportaciones al ISSS en el año.</span>'));
+
+                $('#input-box-1').fadeOut(500, function() {
+                    $('#input-box-2').fadeIn(500);
+                });
+            }
+            
+            if (option == 3) {
+                $('#note-2').html($('#note-2').text().replace($('#note-2').text(), '<span class="highlight-blue">Tus gastos en el año.</span> <br><br>Olvídate de los $800 de colegiatura y los $800 de salud, esos ya los tome en cuenta.'));
+
+                $('#input-box-1').fadeOut(500, function() {
+                    $('#input-box-2').fadeIn(500);
+                });
             }
         }
     });
+
+    $('#input-2').on('keypress',function(e) {
+        expenses = $('#input-2').val();
+        if (expenses != '') {
+            expenses = parseFloat(expenses);
+        }
+
+        if(e.which == 13)  {
+
+            if (option == 2) {
+                $('#note-3').html($('#note-3').text().replace($('#note-2').text(), '<span class="highlight-blue">Tus aportaciones al AFP en el año.</span>'));
+
+                $('#input-box-1').fadeOut(500, function() {
+                    $('#input-box-2').fadeIn(500);
+                });
+            }
+            
+            if (option == 3 && income != null) {
+                $('#results-header').html($('#results-header').text().replace($('#results-header').text(), resultsHeader(option, income, expenses, health, retirement, taxes)));
+                $('#results-text').html($('#results-text').text().replace($('#results-text').text(), resultsText2(impuestos_anual(income, expenses, health, retirement, taxes))));
+                
+                $('#input-page').fadeOut(500, function(){
+                    $('#results-page').fadeIn(500);
+                })
+                document.activeElement.blur();
+            }
+        }
+
+    });
+
     
     $('#reset').click(function(){
         $('#results-page').fadeOut(500, function() {
             $('#starting-page').fadeIn(500);
         });
         $('#input-1').val('');
+        $('#input-2').val('');
+        $('#input-3').val('');
+        $('#input-4').val('');
+        $('#input-box-1').show();
+        $('#input-box-2').hide();
+        $('#input-box-3').hide();
+        $('#input-box-4').hide();
     });
 
 });
